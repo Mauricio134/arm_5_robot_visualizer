0c948ecee171533f2753b95a03df33f4f6a56173
Mauricio David Apaza Iruri <mauricio.apaza@stepsconsulting.com>
2025-08-14 18:02:34 +0200

    Updated changes and path

--- func.py
+++ func.py
@@ -1,14 +1,10 @@
 import math
-import time
+import matplotlib.pyplot as plt
 
-# Functions
-def findD(x1, y1, x2, y2):
-    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-
-def findAngSide(a, b, c):
-    cos_value = (a**2 + b**2 - c**2) / (2 * a * b)
-    cos_value = max(min(cos_value, 1), -1)
-    return math.degrees(math.acos(cos_value))
+def get_euclidean_distance(init, fin):
+    delta_x = (fin[0] - init[0])**2
+    delta_y = (fin[1] - init[1])**2
+    return math.sqrt(delta_x + delta_y)
 
 def get_omegas(arms_size, separation, target_x, target_y):
     L0 = separation / 2
@@ -27,8 +23,31 @@ def get_beta(separation, max_point, min_point, center_1, center_2, target_x, tar
         return [math.degrees(math.pi - math.atan(target_y/abs(L0+target_x))),math.degrees(math.pi - math.atan(target_y/abs(L0 - target_x)))]
     return []
 
+def intersection_points(size, center_1, center_2):
+
+    distance = get_euclidean_distance(center_1, center_2)
+
+    if distance < size*2:
+        distance_a = (size**2 - size**2 + distance**2) / (2*distance)
+        distance_b = distance_a
+
+        distance_h = round(math.sqrt(size**2-distance_a**2), 1)
+
+        point_5 = [0,0]
+
+        point_5[0] = center_1[0] + (distance_a/distance) * (center_2[0] - center_1[0])
+        point_5[1] = center_1[1] + (distance_a/distance) * (center_2[1] - center_1[1])
+
+        perpendicular_1 = [center_2[1] - center_1[1], center_1[0] - center_2[0]]
+        perpendicular_2 = [center_1[1] - center_2[1], center_2[0] - center_1[0]]
+
+        return [[point_5[0] + (distance_h * (perpendicular_1[0]))/(distance), point_5[1] + (distance_h * (perpendicular_1[1]))/(distance)],[point_5[0] + (distance_h * (perpendicular_2[0]))/(distance), point_5[1] + (distance_h * (perpendicular_2[1]))/(distance)]]
+    else:
+        print("There are not interseccioned points")
+        return []
+    
 def get_x_max(y, R, h, k):
-    return [round(math.sqrt(R**2-(y-k)**2)+h), round(-math.sqrt(R**2-(y-k)**2)+h)]
+    return [math.sqrt(R**2-(y-k)**2)+h, -math.sqrt(R**2-(y-k)**2)+h]
 
 def gcd(a,b):
 
@@ -52,73 +71,128 @@ def largest_divisor(n):
         
     return math.sqrt(n)
 
-def intersection_points(size, center_1, center_2):
-
-    distance = findD(center_1[0], center_1[1], center_2[0], center_2[1])
-
-    if distance < size*2:
-        distance_a = (size**2 - size**2 + distance**2) / (2*distance)
-        distance_b = distance_a
-
-        distance_h = round(math.sqrt(size**2-distance_a**2), 1)
-
-        point_5 = [0,0]
-
-        point_5[0] = center_1[0] + (distance_a/distance) * (center_2[0] - center_1[0])
-        point_5[1] = center_1[1] + (distance_a/distance) * (center_2[1] - center_1[1])
-
-        perpendicular_1 = [center_2[1] - center_1[1], center_1[0] - center_2[0]]
-        perpendicular_2 = [center_1[1] - center_2[1], center_2[0] - center_1[0]]
-
-        return [[round(point_5[0] + (distance_h * (perpendicular_1[0]))/(distance)), round(point_5[1] + (distance_h * (perpendicular_1[1]))/(distance))],[round(point_5[0] + (distance_h * (perpendicular_2[0]))/(distance)), round(point_5[1] + (distance_h * (perpendicular_2[1]))/(distance))]]
-    else:
-        print("There are not interseccioned points")
-        return []
-    
 def get_extremes(size, arm, angle):
     extremo_x = arm[0] + size * math.cos(math.radians(angle))
     extremo_y = arm[1] + size * math.sin(math.radians(angle))
 
     return [extremo_x, extremo_y]
 
-def reachibility_path(whole_map, row, column, total_row, total_column, target_x, target_y, radius):
+def get_grid_movement(min_point, max_point, size_whole_arm_array, size_between_motors, base_arm_1, base_arm_2, angles_per_step,simple_or_each, padding_y):
+    whole_map = []
+    whole_degrees = []
+
+    row = 0
+    column = 0
+
+    exact_row = 0
+    exact_column = 0
+
+    min_point = [min_point[0], min_point[1] + padding_y]
+
+    distance_height = get_euclidean_distance([min_point[0], min_point[1]], [min_point[0], max_point[1]])
+    distance_width = get_euclidean_distance([min_point[0], min_point[1]], [max_point[0], min_point[1]])
+
+    num_large_h = largest_divisor(distance_height)
+    num_large_w = largest_divisor(distance_width)
+
+    if(simple_or_each == True):
+        num_large_h = gcd(distance_height, distance_width)
+        num_large_w = num_large_h
+
+    total_row = (-min_point[1]+max_point[1])/num_large_h + 1
+    total_column = (-min_point[0]+max_point[0])/num_large_w + 1
+
+    actual_distance = 999999
+
+    for i in range(int(min_point[1]), int(max_point[1])+1, int(num_large_h)):
+        fila_1 = []
+        fila_2 = []
+        for j in range(int(min_point[0]), int(max_point[0]) +1,int(num_large_w)):
+            try:
+                omegas = get_omegas(size_whole_arm_array, size_between_motors, j, i)
+                betas = get_beta(size_between_motors, max_point, min_point, base_arm_1, base_arm_2, j, i)
+
+                theta_1 = betas[0] + omegas[0]
+                theta_2 = betas[1] - omegas[1]
+
+                if theta_1 % angles_per_step != 0:
+                    theta_1 = round(theta_1/angles_per_step) * angles_per_step
+
+                if theta_2 % angles_per_step != 0:
+                    theta_2 = round(theta_2/angles_per_step) * angles_per_step
+                
+                extremo_1 = get_extremes(size_whole_arm_array[0], base_arm_1, theta_1)
+                extremo_2 = get_extremes(size_whole_arm_array[0], base_arm_2, theta_2)
+
+                intersections = intersection_points(size_whole_arm_array[1], extremo_1, extremo_2)
+                new_distance = get_euclidean_distance([0,min_point[1]],[intersections[1][0], intersections[1][1]])
+                if actual_distance > new_distance:
+                    actual_distance = new_distance
+                    exact_row = row
+                    exact_column = column
+                fila_1.append(intersections[1])
+                fila_2.append([theta_1, theta_2])
+            except:
+                fila_1.append([-999,-999])
+                fila_2.append([])
+            
+            column += 1
+        whole_map.append(fila_1)
+        whole_degrees.append(fila_2)
+        row += 1
+        column = 0
+    return whole_map, whole_degrees, [exact_row, exact_column], [total_row, total_column], [num_large_h, num_large_h]
+
+def reachibility_path(whole_map, init_position, total_row, total_column, target, radius, sizes, size_between_motors, max_point, min_point, arm_1, arm_2, angles_per_step):
     path = []
-    path.append(whole_map[row][column])
-    while(findD(whole_map[row][column][0], whole_map[row][column][1], target_x, target_y) > radius):
-        min_y = row - 1
-        max_y = row + 2
-        min_x = column - 1
-        max_x = column + 2
+    angles = []
+    distances = []
+    while(get_euclidean_distance(whole_map[init_position[0]][init_position[1]], target) > radius):
+        min_y = init_position[0] - 1
+        max_y = init_position[0] + 2
+        min_x = init_position[1] - 1
+        max_x = init_position[1] + 2
 
-        if row - 1 < 0:
-            min_y = row
+        if init_position[0] - 1 < 0:
+            min_y = init_position[0]
         
-        if row + 2 >= total_row:
-            max_y = row + 1
+        if init_position[0] + 2 >= total_row:
+            max_y = init_position[0] + 1
 
-        if column - 1 < 0:
-            min_x = column
+        if init_position[1] - 1 < 0:
+            min_x = init_position[1]
 
-        if column + 2 >= total_column:
-            max_x = column + 1
+        if init_position[1] + 2 >= total_column:
+            max_x = init_position[1] + 1
         
         distance = 9999
         new_row = -999
         new_column = -999
         for i in range(min_y, max_y):
             for j in range(min_x, max_x):
-                if whole_map[i][j][0] == -999 or (whole_map[i][j][0] == whole_map[row][column][0] and whole_map[i][j][1] == whole_map[row][column][1]):
+                if whole_map[i][j][0] == -999 or (whole_map[i][j][0] == whole_map[init_position[0]][init_position[1]][0] and whole_map[i][j][1] == whole_map[init_position[0]][init_position[1]][1]):
                     continue
 
-                new_distance = findD(whole_map[row][column][0], whole_map[row][column][1], whole_map[i][j][0], whole_map[i][j][1]) + findD(whole_map[i][j][0], whole_map[i][j][1], target_x, target_y)
+                new_distance = get_euclidean_distance(whole_map[init_position[0]][init_position[1]], whole_map[i][j]) + get_euclidean_distance(whole_map[i][j], target)
                 if new_distance < distance:
                     distance = new_distance
                     new_row = i
                     new_column = j
         
-        row = new_row
-        column = new_column
-        path.append(whole_map[row][column])
+        init_position[0] = new_row
+        init_position[1] = new_column
 
+        omegas = get_omegas(sizes, size_between_motors, whole_map[init_position[0]][init_position[1]][0], whole_map[init_position[0]][init_position[1]][1])
+        betas = get_beta(size_between_motors, max_point, min_point, arm_1, arm_2, whole_map[init_position[0]][init_position[1]][0], whole_map[init_position[0]][init_position[1]][1])
+
+        theta_1 = betas[0] + omegas[0]
+        theta_2 = betas[1] - omegas[1]
+
+        theta_1 = round(theta_1/angles_per_step) * angles_per_step
+        theta_2 = round(theta_2/angles_per_step) * angles_per_step
+
+        path.append(whole_map[init_position[0]][init_position[1]])
+        angles.append([theta_1, theta_2])
+        distances.append([init_position[0], init_position[1]])
     
-    return path
+    return path, angles, distances
--- model.py
+++ model.py
@@ -1,168 +1,30 @@
-import time
 import func
-import tkinter as tk
-import math
-import matplotlib.pyplot as plt
-import numpy as np
-
-# Features of NEMA 14 Stepper Motor
-rpm = 5
-angles_per_second = rpm * 6
-
-angles_per_second_pos = angles_per_second
-angles_per_second_neg = -angles_per_second
 
 angles_per_step = 1.8
+total_duration_ms = 6000
 
-duration = 6000
+size_lower_arm = 76.34
+size_upper_arm = 94.66
+size_whole_arm = size_lower_arm + size_upper_arm
 
-# Features of five arm parallel robot
-size_a = 76.34
-size_b = 94.66
-base_arm_1_x, base_arm_1_y = -20.5, 0
-base_arm_2_x, base_arm_2_y = 20.5, 0
-size_between_motors = func.findD(base_arm_2_x, base_arm_2_y, base_arm_1_x, base_arm_1_y)
+size_whole_arm_array = [size_lower_arm, size_upper_arm]
 
-# Features of target
-init_target_x, init_target_y = 0, base_arm_1_y + 150
-fin_target_x, fin_target_y = 130, 72
+base_arm_1 = [-20.5, 0]
+base_arm_2 = [20.5, 0]
 
-# Init Angles
-# Faces
-init_face_1 = func.findD(base_arm_1_x, base_arm_1_y, init_target_x, init_target_y)
-init_face_2 = func.findD(base_arm_2_x, base_arm_2_y, init_target_x, init_target_y)
+size_between_motors = func.get_euclidean_distance(base_arm_1, base_arm_2)
 
-# Angles in motor 1
-init_theta_2 = func.findAngSide(size_a, init_face_1, size_b)
-init_theta_3 = func.findAngSide(init_face_1, size_between_motors, init_face_2)
-init_theta_total = init_theta_2 + init_theta_3
-
-# Angles in motor 2
-init_beta_2 = func.findAngSide(size_a, init_face_2, size_b)
-init_beta_3 = func.findAngSide(init_face_2, size_between_motors, init_face_1)
-init_beta_1 = 180.0 - (init_beta_3 + init_beta_2)
-
-# End Angles
-# Faces
-fin_face_1 = func.findD(base_arm_1_x, base_arm_1_y, fin_target_x, fin_target_y)
-fin_face_2 = func.findD(base_arm_2_x, base_arm_2_y, fin_target_x, fin_target_y)
-
-# Angles in motor 1
-fin_theta_2 = func.findAngSide(size_a, fin_face_1, size_b)
-fin_theta_3 = func.findAngSide(fin_face_1, size_between_motors, fin_face_2)
-fin_theta_total = fin_theta_2 + fin_theta_3
-
-# Angles in motor 2
-fin_beta_2 = func.findAngSide(size_a, fin_face_2, size_b)
-fin_beta_3 = func.findAngSide(fin_face_2, size_between_motors, fin_face_1)
-fin_beta_1 = 180.0 - (fin_beta_3 + fin_beta_2)
-
-# New version of angles
-
-minimum_y = base_arm_1_y + 30
-maximum_y = func.intersection_points(size_a + size_b, [base_arm_1_x, base_arm_1_y], [base_arm_2_x, base_arm_2_y])[1][1]
-
-maximum_x = func.get_x_max(0, size_a + size_b, base_arm_1_x, base_arm_1_y)[0]
+minimum_y = base_arm_1[1]
+maximum_y = func.intersection_points(size_whole_arm, base_arm_1, base_arm_2)[1][1]
+maximum_x = func.get_x_max(0, size_whole_arm, base_arm_1[0], base_arm_1[1])[0]
 minimum_x = -maximum_x
 
 max_point = [maximum_x, maximum_y]
 min_point = [minimum_x, minimum_y]
 
-# omegas = func.get_omegas([size_a, size_b], size_between_motors, fin_target_x, fin_target_y)
-# betas = func.get_beta(size_between_motors, max_point, min_point, [base_arm_1_x, base_arm_1_y], [base_arm_2_x, base_arm_2_y], fin_target_x, fin_target_y)
+target = [-60, 100]
 
-# theta_1 = betas[0] + omegas[0]
-# theta_2 = betas[1] - omegas[1]
+whole_map, whole_degrees, exact_point, dimensions, separations = func.get_grid_movement(min_point, max_point, size_whole_arm_array, size_between_motors, base_arm_1, base_arm_2, angles_per_step, False, 38)
 
-# if theta_1 % angles_per_step != 0:
-#     theta_1 -= theta_1 % angles_per_step
+path, _, l = func.reachibility_path(whole_map, exact_point, dimensions[0], dimensions[1], target,min(separations[0], separations[1]), size_whole_arm_array, size_between_motors, max_point, min_point, base_arm_1, base_arm_2, angles_per_step)
 
-# if theta_2 % angles_per_step != 0:
-#     theta_2 -= theta_2 % angles_per_step
-
-# extremo_1 = func.get_extremes(size_a, [base_arm_1_x, base_arm_1_y], theta_1)
-# extremo_2 = func.get_extremes(size_a, [base_arm_2_x, base_arm_2_y], theta_2)
-
-# intersections = func.intersection_points(size_b, extremo_1, extremo_2)
-
-# fin_target_x = intersections[1][0]
-# fin_target_y = intersections[1][1]
-
-distance_height = func.findD(0,minimum_y, 0, maximum_y)
-distance_width = func.findD(minimum_x, minimum_y, maximum_x, minimum_y)
-
-simple_or_each = False
-
-num_large_h = func.largest_divisor(distance_height)
-num_large_w = func.largest_divisor(distance_width)
-
-if(simple_or_each == True):
-    num_large_h = func.gcd(distance_height, distance_width)
-    num_large_w = num_large_h
-
-# extremo_1 = func.get_extremes(size_a, [base_arm_1_x, base_arm_1_y], theta_1)
-# extremo_2 = func.get_extremes(size_a, [base_arm_2_x, base_arm_2_y], theta_2)
-
-# intersections = func.intersection_points(size_b, extremo_1, extremo_2)
-
-# print(intersections[1])
-
-mapped_x = []
-mapped_y = []
-
-whole_map = []
-
-row = 0
-column = 0
-
-exact_row = 0
-exact_column = 0
-
-total_row = (-minimum_y+maximum_y)/num_large_h + 1
-total_column = (-minimum_x+maximum_x)/num_large_w + 1
-
-for i in range(minimum_y, maximum_y+1, num_large_h):
-    fila = []
-    for j in range(minimum_x, maximum_x +1, num_large_w):
-        try:
-            omegas = func.get_omegas([size_a, size_b], size_between_motors, j, i)
-            betas = func.get_beta(size_between_motors, max_point, min_point, [base_arm_1_x, base_arm_1_y], [base_arm_2_x, base_arm_2_y], j, i)
-
-            theta_1 = betas[0] + omegas[0]
-            theta_2 = betas[1] - omegas[1]
-
-            if theta_1 % angles_per_step != 0:
-                theta_1 = round(theta_1/angles_per_step) * angles_per_step
-
-            if theta_2 % angles_per_step != 0:
-                theta_2 = round(theta_2/angles_per_step) * angles_per_step
-            
-            extremo_1 = func.get_extremes(size_a, [base_arm_1_x, base_arm_1_y], theta_1)
-            extremo_2 = func.get_extremes(size_a, [base_arm_2_x, base_arm_2_y], theta_2)
-
-            intersections = func.intersection_points(size_b, extremo_1, extremo_2)
-            mapped_x.append(intersections[1][0])
-            mapped_y.append(intersections[1][1])
-            if intersections[1][0] == 0 and whole_map[exact_row][exact_column] > intersections[1][1]:
-                exact_row = row
-                exact_column = column
-            fila.append(intersections[1])
-        except:
-            mapped_x = mapped_x
-            mapped_y = mapped_y
-            fila.append([-999,-999])
-        
-        column += 1
-    whole_map.append(fila)
-    row += 1
-    column = 0
-print(whole_map[exact_row][exact_column])
-plt.plot(whole_map[exact_row][exact_column][0], whole_map[exact_row][exact_column][1], 'x')
-plt.plot(mapped_x, mapped_y, 'o')
-plt.plot([base_arm_1_x, base_arm_2_x], [base_arm_1_y, base_arm_2_y], 'x')
-plt.plot(fin_target_x, fin_target_y, 'x')
-plt.show()
-
-accesability_path = func.reachibility_path(whole_map, exact_row, exact_column, total_row, total_column, fin_target_x, fin_target_y, min(num_large_h, num_large_w))
-
-print(accesability_path)
